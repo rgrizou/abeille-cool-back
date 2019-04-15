1a00baf7d1e55d7e4b6288158624d20e6d23b7f9
RomainGrizou <rgrizou@gmail.com>
2019-04-15 17:49:35 +0200

    Base projet abeille cool back

---
+++ src/main/java/abeille/cool/AbeilleCoolBackApplication.java
@@ -0,0 +1,13 @@
+package abeille.cool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbeilleCoolBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbeilleCoolBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/abeille/cool/AbeilleCoolBackApplicationTests.java
@@ -0,0 +1,16 @@
+package abeille.cool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AbeilleCoolBackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
