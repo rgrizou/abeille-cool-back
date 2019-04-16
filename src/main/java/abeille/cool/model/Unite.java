39c5c3b768da9da8fe530ea9f1cd0fd89c0af2d3
Chataigneau <adri.chataigneau@gmail.com>
2019-04-16 15:23:24 +0200

    Correction GROSSE ERREUR DE Gégé

--- src/main/java/abeille/cool/model/Unite.java
+++ src/main/java/abeille/cool/model/Unite.java
@@ -1,17 +1,18 @@
 package abeille.cool.model;
 
 public enum Unite {
+		
+	Unite("Unite"), cL("Centilitre"), kg("Kilogramme"), g("Gramme");
 	
-Unite("Unite"), cL("Centilitre"), kg("Kilogramme"), g("Gramme");
-
-private final String label;
-private Unite(String label) {
-this.label = label;	}
-
-public String getLabel() {
-return label;
-		}
-
+	private final String label;
+	private Unite(String label) {
+		this.label = label;	
 	}
+	
+	public String getLabel() {
+		return label;
+	}
+
+}
 
 
--- src/main/java/abeille/cool/repository/ICommandeClientRepository.java
+++ src/main/java/abeille/cool/repository/ICommandeClientRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import abeille.cool.model.CommandeClient;
 
-public interface ICommandeClient extends JpaRepository<CommandeClient, Long>{
+public interface ICommandeClientRepository extends JpaRepository<CommandeClient, Long>{
 
 }
