674549c9996a09c9137cc94f02958d409b18a680
Jordi <jordisarpoulet@hotmail.com>
2019-04-17 14:38:44 +0200

    Methodes repo fournisseur produits

--- src/main/java/abeille/cool/repository/IProduitRepository.java
+++ src/main/java/abeille/cool/repository/IProduitRepository.java
@@ -2,8 +2,11 @@ package abeille.cool.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import abeille.cool.model.Produit;
 
 public interface IProduitRepository extends JpaRepository <Produit, Long> {
 
+	
+	
 }
