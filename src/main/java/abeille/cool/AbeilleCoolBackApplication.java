86bc48a1e7679558342e4c1d59f4d6f64f5b22ff
Maud <maudrinfray@hotmail.fr>
2019-04-17 16:25:14 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/abeille/cool/AbeilleCoolBackApplication.java
+++ src/main/java/abeille/cool/AbeilleCoolBackApplication.java
@@ -1,12 +1,17 @@
 package abeille.cool;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
 
 @SpringBootApplication
 public class AbeilleCoolBackApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AbeilleCoolBackApplication.class, args);
 	}
 
